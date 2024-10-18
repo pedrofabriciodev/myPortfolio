039ba3db9c56eab23cd961fa76e758d1a6e1389c
Pedro Fabricio <pedrofabriciodev@gmail.com>
2024-10-18 16:10:41 -0300

    feat: config material ui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@emotion/react'
+import theme from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
