11b9fa99ed7f2df00d9533c3af9d73bce789e385
Pedro Fabricio <pedrofabriciodev@gmail.com>
2024-10-18 19:00:24 -0300

    feat: add hero

---
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,70 @@
+
+import Avatar from "../../../../assets/images/octocat-1725150690878.png"
+
+import Grid from '@mui/material/Grid2';
+import { Container, styled, Typography } from "@mui/material";
+
+import DownloadIcon from '@mui/icons-material/Download';
+import EmailIcon from '@mui/icons-material/Email';
+import StyledButton from "../../../../components/button/StyledButton";
+
+
+
+const Hero = () => {
+
+    return (
+      <>
+
+        <StyledHero>
+            <Container maxWidth= 'lg'>
+                <Grid container spacing={2}>
+                    <Grid size={{ xs: 12, md: 5 }} display={"flex"} justifyContent={"center"}>
+                      <StyleAvatar src={Avatar} />
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 7 }}>
+                      <Typography color="primary.contrastText" variant="h1" textAlign={"center"}>Pedro Fabricio</Typography>
+                      <Typography color="primary.contrastText" variant="h2" textAlign={"center"}>I'm a Software Engineer</Typography>
+
+
+                      <Grid container display={"flex"} justifyContent={"center"} spacing={3}>
+
+                        <Grid size={{ xs: 12, md: 4 }} display={"flex"} justifyContent={"center"}>
+                          <StyledButton>  
+                            <DownloadIcon/>
+                            <Typography>Baixar CV</Typography>
+                          </StyledButton>
+                        </Grid>
+
+                        <Grid size={{ xs: 12, md: 4 }} display={"flex"} justifyContent={"center"}>
+                          <StyledButton>
+                            <EmailIcon/>
+                            <Typography>Contact Me</Typography>
+                          </StyledButton>
+                        </Grid>
+
+
+                      </Grid>
+
+                    </Grid>
+                </Grid>
+            </Container>
+
+        </StyledHero>
+      </>
+    )
+  }
+
+  const StyledHero = styled("div")(({ theme }) => ({
+    height: "100vh",
+    backgroundColor: theme.palette.primary.main,
+    display:"flex",
+    alignItems: "center"
+  }));
+  
+
+  const StyleAvatar = styled("img")(()=> ({
+    width:"80%"
+  }))
+
+  export default Hero
+  
