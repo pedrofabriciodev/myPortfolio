15cf6e2d19ac8e9987e6ee2184605ea5fb7a0ad7
Pedro Fabricio <pedrofabriciodev@gmail.com>
2024-12-17 11:53:00 -0300

    fix: drive link changed

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -12,7 +12,7 @@ import StyledButton from "../../../../components/button/StyledButton";
 
 const Hero = () => {
 
-  const fileUrl = "https://drive.google.com/file/d/1G6OAeT9ykzOsxSbFlMyx9Pg1gg8t01vA/view?usp=sharing";
+  const fileUrl = "https://drive.google.com/file/d/1D5I3xFk9RZSDHmJ5_R5YSl-ycjmUUFW6/view?usp=sharing";
     return (
       <>
 
